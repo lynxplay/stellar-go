af9f5fcf3673fca3245c503d0f40985f4fd29d1f
Alfonso Acosta <alfonso@stellar.org>
2022-04-19 04:35:16 +0200

    Implement mocks by pointer

--- services/horizon/internal/db2/history/mock_q_txsub_result.go
+++ services/horizon/internal/db2/history/mock_q_txsub_result.go
@@ -13,27 +13,27 @@ type MockQTxSubmissionResult struct {
 	mock.Mock
 }
 
-func (m MockQTxSubmissionResult) TxSubGetResult(ctx context.Context, hash string) (Transaction, error) {
+func (m *MockQTxSubmissionResult) TxSubGetResult(ctx context.Context, hash string) (Transaction, error) {
 	a := m.Called(ctx, hash)
 	return a.Get(0).(Transaction), a.Error(1)
 }
 
-func (m MockQTxSubmissionResult) TxSubSetResult(ctx context.Context, transaction ingest.LedgerTransaction, sequence uint32, ledgerClosetime time.Time) error {
+func (m *MockQTxSubmissionResult) TxSubSetResult(ctx context.Context, transaction ingest.LedgerTransaction, sequence uint32, ledgerClosetime time.Time) error {
 	a := m.Called(ctx, transaction, sequence)
 	return a.Error(0)
 }
 
-func (m MockQTxSubmissionResult) TxSubInit(ctx context.Context, hash string) error {
+func (m *MockQTxSubmissionResult) TxSubInit(ctx context.Context, hash string) error {
 	a := m.Called(ctx, hash)
 	return a.Error(0)
 }
 
-func (m MockQTxSubmissionResult) TxSubDeleteOlderThan(ctx context.Context, howOldInSeconds uint64) (int64, error) {
+func (m *MockQTxSubmissionResult) TxSubDeleteOlderThan(ctx context.Context, howOldInSeconds uint64) (int64, error) {
 	a := m.Called(ctx, howOldInSeconds)
 	return a.Get(0).(int64), a.Error(1)
 }
 
-func (m MockQTxSubmissionResult) TxSubGetResults(ctx context.Context, hashes []string) ([]Transaction, error) {
+func (m *MockQTxSubmissionResult) TxSubGetResults(ctx context.Context, hashes []string) ([]Transaction, error) {
 	a := m.Called(ctx, hashes)
 	return a.Get(0).([]Transaction), a.Error(1)
 }
